fbe7f11a2ff40dea1f1af1b97dced9508dcbb484
Ilya Mafov <meat.100@gmail.com>
2021-03-14 14:39:50 +0300

    Hotfix

--- src/main/resources/db/changelog/sql/001_create_all_tables.sql
+++ src/main/resources/db/changelog/sql/001_create_all_tables.sql
@@ -1,7 +1,8 @@
 CREATE TABLE users (
     id                  BIGSERIAL PRIMARY KEY,
     login               VARCHAR(50) UNIQUE,
-    password            VARCHAR(50) NOT NULL
+    password            VARCHAR(255) NOT NULL,
+    enabled             BOOLEAN DEFAULT TRUE
 );
 
 CREATE TABLE staff_units (

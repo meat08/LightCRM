a2a485923dc848098ffb9124150ed014e7d468d6
Ilya Mafov <meat.100@gmail.com>
2021-03-15 14:30:42 +0300

    Добавил index.html, main.html и index.js

---
+++ src/main/resources/static/index.js
@@ -0,0 +1,32 @@
+(function () {
+    'use strict';
+
+    //Какие-то из модулей могут быть не нужны. Лишнее выпилим при рефакторинге
+    angular
+        .module('app', ['ngRoute', 'ngStorage', 'angular-jwt', 'ngAnimate', 'ngSanitize', 'ui.bootstrap'])
+        .config(config)
+        .run(run);
+
+    function config($routeProvider, $httpProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'main/main.html'
+            });
+    }
+
+    //Функция проверяет наличие пользователя в локальном хранилище и клеит токен к заголовку
+    function run($rootScope, $http, $location, $localStorage) {
+        if ($localStorage.currentUser) {
+            $http.defaults.headers.common.Authorization = 'Bearer ' + $localStorage.currentUser.token;
+        }
+        //Доступ без токена возможен только на страницы в массиве publicPages,
+        // все остальное перенаправляется на /auth
+        $rootScope.$on('$locationChangeStart', function (event, next, current) {
+            let publicPages = ['/auth'];
+            let restrictedPage = publicPages.indexOf($location.path()) === -1;
+            if (restrictedPage && !$localStorage.currentUser) {
+                $location.path('/auth');
+            }
+        });
+    }
+})();

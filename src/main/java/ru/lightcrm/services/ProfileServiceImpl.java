6a4329a679af2dc5fa4ba973e939ea52aecfc7af
Denis Anisimov <anisimov.denis.90@gmail.com>
2021-03-16 23:05:27 +0300

    dev-35: Сервис сущности Профиль. Добавлены тесты, бросается эксепшен, если не найден Профиль по id

--- src/main/java/ru/lightcrm/services/ProfileServiceImpl.java
+++ src/main/java/ru/lightcrm/services/ProfileServiceImpl.java
@@ -3,6 +3,7 @@ package ru.lightcrm.services;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.lightcrm.entities.dtos.ProfileDto;
+import ru.lightcrm.exceptions.ResourceNotFoundException;
 import ru.lightcrm.repositories.ProfileRepository;
 import ru.lightcrm.services.interfaces.ProfileService;
 
@@ -16,11 +17,11 @@ public class ProfileServiceImpl implements ProfileService {
 
     @Override
     public ProfileDto findById(Long id) {
-        return new ProfileDto(profileRepository.findById().get());
+        return new ProfileDto(profileRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(String.format("Профиль с id %s отсутствует", id))));
     }
 
     @Override
     public List<ProfileDto> findAll() {
-        return profileRepository.findAll().stream.map(ProfileDto::new).collect(Collectors.toList());
+        return profileRepository.findAll().stream().map(ProfileDto::new).collect(Collectors.toList());
     }
 }

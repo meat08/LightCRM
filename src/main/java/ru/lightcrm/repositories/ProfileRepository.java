7572a69a2720136343ccc704927cde0587346912
Denis Anisimov <anisimov.denis.90@gmail.com>
2021-03-16 21:02:20 +0300

    dev-28: Репозиторий сущности Профиль

---
+++ src/main/java/ru/lightcrm/repositories/ProfileRepository.java
@@ -0,0 +1,16 @@
+package ru.lightcrm.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import ru.lightcrm.entities.Profile;
+
+import java.util.Optional;
+
+@Repository
+public interface ProfileRepository extends JpaRepository<Profile, Long>, JpaSpecificationExecutor<Profile> {
+
+    @Query("SELECT p FROM Profile p WHERE p.user.login = ?1")
+    Optional<Profile> findByLogin(String login);
+}

14e9ba4759662c5031b2a20ee99ac50a8bb7eea5
Denis Anisimov <anisimov.denis.90@gmail.com>
2021-03-16 23:48:17 +0300

    dev-42: Контроллер Профилей. Добавлены тесты.

--- src/main/java/ru/lightcrm/controllers/interfaces/ProfileController.java
+++ src/main/java/ru/lightcrm/controllers/interfaces/ProfileController.java
@@ -12,12 +12,19 @@ public interface ProfileController {
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "OK", response = ProfileDto.class),
             @ApiResponse(code = 400, message = "Указан некорректны id"),
-            @ApiResponse(code = 404, message = "Профиль с указанным id не найден")
+            @ApiResponse(code = 401, message = "Клиент не авторизован"),
+            @ApiResponse(code = 403, message = "Нет прав"),
+            @ApiResponse(code = 404, message = "Профиль с указанным id отсутствует")
     })
     ProfileDto getById(@ApiParam(value = "Уникальный идентификатор профиля", name = "id", required = true, example = "1") Long id);
 
     @ApiOperation(value = "Возвращает список всех профилей", httpMethod = "GET", produces = "application/json", response = ProfileDto.class, responseContainer = "List")
-    @ApiResponses(value = @ApiResponse(code = 200, message = "OK", response = ProfileDto.class, responseContainer = "List"))
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK", response = ProfileDto.class, responseContainer = "List"),
+            @ApiResponse(code = 401, message = "Клиент не авторизован"),
+            @ApiResponse(code = 403, message = "Нет прав"),
+            @ApiResponse(code = 404, message = "Ресурс отсутствует")
+    })
     List<ProfileDto> getAll();
 
 }

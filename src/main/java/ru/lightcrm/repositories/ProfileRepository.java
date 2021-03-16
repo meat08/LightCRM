c74721e77dcde7e41e2cf38523eef8d975e80ae5
Denis Anisimov <anisimov.denis.90@gmail.com>
2021-03-16 20:25:53 +0300

    dev-28: Доработка репозитория: удалены JOIN FETCH

--- src/main/java/ru/lightcrm/repositories/ProfileRepository.java
+++ src/main/java/ru/lightcrm/repositories/ProfileRepository.java
@@ -6,37 +6,11 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.lightcrm.entities.Profile;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ProfileRepository extends JpaRepository<Profile, Long>, JpaSpecificationExecutor<Profile> {
-    @Query("SELECT p FROM Profile p " +
-            "JOIN FETCH p.user " +
-            "JOIN FETCH p.staffUnit " +
-            "JOIN FETCH p.managedDepartment " +
-            "JOIN FETCH p.departments " +
-            "JOIN FETCH p.company " +
-            "JOIN FETCH p.comments " +
-            "WHERE p.user.login = ?1")
+    
+    @Query("SELECT p FROM Profile p WHERE p.user.login = ?1")
     Optional<Profile> findByLogin(String login);
-
-    @Query("SELECT p FROM Profile p " +
-            "JOIN FETCH p.user " +
-            "JOIN FETCH p.staffUnit " +
-            "JOIN FETCH p.managedDepartment " +
-            "JOIN FETCH p.departments " +
-            "JOIN FETCH p.company " +
-            "JOIN FETCH p.comments " +
-            "WHERE p.id = ?1")
-    Optional<Profile> findById(Long id);
-
-    @Query("SELECT p FROM Profile p " +
-            "JOIN FETCH p.user " +
-            "JOIN FETCH p.staffUnit " +
-            "JOIN FETCH p.managedDepartment " +
-            "JOIN FETCH p.departments " +
-            "JOIN FETCH p.company " +
-            "JOIN FETCH p.comments")
-    List<Profile> findAll();
 }

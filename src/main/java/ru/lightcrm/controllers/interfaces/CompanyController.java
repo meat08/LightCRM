52a61a5a8f8fbbb45669cb3712be65ca70ffb922
STM <Lyaschenkoilja@gmail.com>
2021-03-23 00:59:42 +0300

    Добавил тест на CompanyService Дополнил описание CompanyController

--- src/main/java/ru/lightcrm/controllers/interfaces/CompanyController.java
+++ src/main/java/ru/lightcrm/controllers/interfaces/CompanyController.java
@@ -7,13 +7,19 @@ import ru.lightcrm.entities.dtos.CompanyDto;
 
 import java.util.List;
 
-@Api(value = "Компании")
+@Api(value = "/api/v1/companies", tags = "Контроллер для работы с компаниями", produces = "application/json")
 public interface CompanyController {
     @GetMapping("/api/v1/companies")
     @ApiOperation(value = " Возвращает cписок DTO компаний",
             notes = "Запрос списка компаний",
             httpMethod = "GET"
     )
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK", response = CompanyDto.class, responseContainer = "List"),
+            @ApiResponse(code = 401, message = "Клиент не авторизован"),
+            @ApiResponse(code = 403, message = "Нет прав"),
+            @ApiResponse(code = 404, message = "Ресурс не найден")
+    })
     List<CompanyDto> getCompanyContent();
 
 }

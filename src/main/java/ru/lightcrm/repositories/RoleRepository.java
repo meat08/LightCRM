e9dc1d7bce522d707de143982e8a6be80430103c
Strantonvad <nottna@gmail.com>
2021-03-26 12:50:36 +0300

    Dev-85. Добавить в репозитории аннотацию @NotNull

--- src/main/java/ru/lightcrm/repositories/RoleRepository.java
+++ src/main/java/ru/lightcrm/repositories/RoleRepository.java
@@ -3,9 +3,9 @@ package ru.lightcrm.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.lightcrm.entities.Role;
 
+import javax.validation.constraints.NotNull;
 import java.util.Optional;
 
-
 public interface RoleRepository extends JpaRepository<Role, Long> {
-    Optional<Role>  findOneByName(String name);
+    Optional<Role> findOneByName(@NotNull String name);
 }

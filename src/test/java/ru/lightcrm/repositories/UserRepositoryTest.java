e3669c746ea5073f9951a69c5668c829bac41b18
Strantonvad <nottna@gmail.com>
2021-03-26 17:35:07 +0300

    Dev-101. Тесты. Добавить тест для UserRepository

---
+++ src/test/java/ru/lightcrm/repositories/UserRepositoryTest.java
@@ -0,0 +1,34 @@
+package ru.lightcrm.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import ru.lightcrm.entities.User;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+public class UserRepositoryTest {
+
+  private final String TEST_LOGIN = "admin";
+
+  @Autowired
+  private UserRepository userRepository;
+
+
+  @Test
+  public void findByLoginTest() {
+    Optional<User> user = userRepository.findByLogin(TEST_LOGIN);
+
+    assertNotNull(user.get());
+    assertTrue(user.get().getId() > 0);
+    assertEquals(TEST_LOGIN, user.get().getLogin().toLowerCase());
+  }
+
+  @Test
+  public void existsByLoginTest() {
+    assertTrue(userRepository.existsByLogin(TEST_LOGIN));
+  }
+}

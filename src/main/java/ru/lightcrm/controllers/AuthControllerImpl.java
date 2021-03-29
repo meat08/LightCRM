2a094611f3429b434c7248ee2725a25d1f29023d
YulDmi <barsetca@rambler.ru>
2021-03-29 19:06:39 +0300

    вынесла бизнес логику из AuthControllerImpl в UserService

--- src/main/java/ru/lightcrm/controllers/AuthControllerImpl.java
+++ src/main/java/ru/lightcrm/controllers/AuthControllerImpl.java
@@ -4,15 +4,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import ru.lightcrm.configs.JwtTokenUtil;
 import ru.lightcrm.controllers.interfaces.AuthController;
-import ru.lightcrm.entities.User;
 import ru.lightcrm.exceptions.LightCrmError;
 import ru.lightcrm.exceptions.ResourceNotFoundException;
 import ru.lightcrm.services.interfaces.UserService;
@@ -26,25 +21,16 @@ import java.util.NoSuchElementException;
 @Slf4j
 public class AuthControllerImpl implements AuthController {
 
-    private final AuthenticationManager authenticationManager;
-    private final JwtTokenUtil jwtTokenUtil;
     private final UserService userService;
 
+    @Override
     public ResponseEntity<?> createAuthToken(@RequestBody JwtRequest jwtRequest) {
-        String username = jwtRequest.getUsername();
+
         try {
-            User user = userService.getByUsername(username);
-            if (!user.isEnabled()) {
-                log.warn("User with login: {} deleted", username);
-                return new ResponseEntity<>(
-                        new LightCrmError(HttpStatus.UNAUTHORIZED.value(), "Account deleted"),
-                        HttpStatus.UNAUTHORIZED);
-            }
-            authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(jwtRequest.getUsername(),
-                            jwtRequest.getPassword()));
+            JwtResponse jwtResponse = userService.createAuthToken(jwtRequest);
+            return ResponseEntity.ok(jwtResponse);
         } catch (BadCredentialsException | NoSuchElementException ex) {
-            log.warn("User with login: {} has incorrect username or password  ", username);
+            log.warn("User with login: {} has incorrect username or password  ", jwtRequest.getUsername());
             return new ResponseEntity<>(
                     new LightCrmError(HttpStatus.UNAUTHORIZED.value(), "Incorrect username or password"),
                     HttpStatus.UNAUTHORIZED);
@@ -53,9 +39,5 @@ public class AuthControllerImpl implements AuthController {
                     new LightCrmError(HttpStatus.NOT_FOUND.value(), e.getMessage()),
                     HttpStatus.NOT_FOUND);
         }
-        UserDetails userDetails = userService.loadUserByUsername(username);
-        String token = jwtTokenUtil.generateToken(userDetails);
-        log.info("Successfully created token for user login {}", username);
-        return ResponseEntity.ok(new JwtResponse(token));
     }
 }
--- src/main/java/ru/lightcrm/services/interfaces/UserService.java
+++ src/main/java/ru/lightcrm/services/interfaces/UserService.java
@@ -3,10 +3,13 @@ package ru.lightcrm.services.interfaces;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import ru.lightcrm.entities.User;
 import ru.lightcrm.entities.dtos.UserDto;
+import ru.lightcrm.utils.JwtRequest;
+import ru.lightcrm.utils.JwtResponse;
 
 public interface UserService extends UserDetailsService {
     UserDto getDtoByUsername(String username);
     User getByUsername(String username);
     void saveUser(User user);
     boolean isPresent(String login);
+    JwtResponse createAuthToken(JwtRequest jwtRequest);
 }

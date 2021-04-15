d2a120d9d89476db23dc8d368555a10c3100b164
YulDmi <barsetca@rambler.ru>
2021-04-15 19:26:55 +0300

    126 добавила методы удаления, добавления, изменения StaffUnit в контроллере и сервисе. + Тесты.

--- src/main/java/ru/lightcrm/controllers/StaffUnitControllerImpl.java
+++ src/main/java/ru/lightcrm/controllers/StaffUnitControllerImpl.java
@@ -1,19 +1,42 @@
 package ru.lightcrm.controllers;
 
+import java.util.HashSet;
 import java.util.List;
+
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RestController;
 import ru.lightcrm.controllers.interfaces.StaffUnitController;
+import ru.lightcrm.entities.dtos.StaffUnitDto;
 import ru.lightcrm.services.interfaces.StaffUnitService;
 
 @RestController
 @RequiredArgsConstructor
 public class StaffUnitControllerImpl implements StaffUnitController {
 
-  private final StaffUnitService staffUnitService;
+    private final StaffUnitService staffUnitService;
 
-  @Override
-  public List<String> getAllStaffUnitNames() {
-    return staffUnitService.getNames();
-  }
+    @Override
+    public List<String> getAllStaffUnitNames() {
+        return staffUnitService.getNames();
+    }
+
+    @Override
+    public StaffUnitDto saveStaffUnit(StaffUnitDto staffUnitDto) {
+        staffUnitDto.setId(null);
+        return staffUnitService.saveOrUpdate(staffUnitDto);
+    }
+
+    @Override
+    public StaffUnitDto updateStuffUnit(StaffUnitDto staffUnitDto) {
+        return staffUnitService.saveOrUpdate(staffUnitDto);
+    }
+
+
+    @Override
+    public void deleteStaffUnitById(Long id) {
+
+        staffUnitService.deleteById(id);
+    }
 }
--- src/main/java/ru/lightcrm/services/StaffUnitServiceImpl.java
+++ src/main/java/ru/lightcrm/services/StaffUnitServiceImpl.java
@@ -1,19 +1,24 @@
 package ru.lightcrm.services;
 
-import java.util.List;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.lightcrm.entities.Role;
 import ru.lightcrm.entities.StaffUnit;
 import ru.lightcrm.entities.dtos.StaffUnitDto;
 import ru.lightcrm.exceptions.ResourceNotFoundException;
 import ru.lightcrm.repositories.StaffUnitRepository;
+import ru.lightcrm.services.interfaces.RoleService;
 import ru.lightcrm.services.interfaces.StaffUnitService;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class StaffUnitServiceImpl implements StaffUnitService {
     private final StaffUnitRepository staffUnitRepository;
+    private final RoleService roleService;
 
     @Override
     public StaffUnit findByName(String name) {
@@ -27,6 +32,31 @@ public class StaffUnitServiceImpl implements StaffUnitService {
 
     @Override
     public List<String> getNames() {
-        return staffUnitRepository.findAll().stream().map(su -> su.getName()).collect(Collectors.toList());
+        return staffUnitRepository.findAll().stream().map(StaffUnit::getName).collect(Collectors.toList());
+    }
+
+    @Override
+    public StaffUnitDto saveOrUpdate(StaffUnitDto staffUnitDto) {
+        StaffUnit staffUnit;
+        if (staffUnitDto.getId() == null) {
+            staffUnit = new StaffUnit();
+
+        } else {
+            staffUnit = staffUnitRepository.findOneByName(staffUnitDto.getName())
+                    .orElseThrow(() -> new ResourceNotFoundException(String.format("Должность %s не найдена", staffUnitDto.getName())));
+        }
+        staffUnit.setName(staffUnitDto.getName());
+
+        Set<Role> roles = null;
+        if (!staffUnitDto.getRoles().isEmpty()) {
+         roles = staffUnitDto.getRoles().stream().map(roleDto -> roleService.findRoleByName(roleDto.getName())).collect(Collectors.toSet());
+       }
+        staffUnit.setRoles(roles);
+        return new StaffUnitDto(staffUnitRepository.save(staffUnit));
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        staffUnitRepository.deleteById(id);
     }
 }
